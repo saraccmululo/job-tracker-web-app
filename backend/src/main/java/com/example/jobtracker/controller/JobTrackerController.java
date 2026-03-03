63e2f7a2359adb63b07b685fc2f9a9216aa8a721
Sara Costa Cabral Mululo <saramululo@gmail.com>
2026-03-03 15:27:49 -0600

    create controller layer

--- backend/src/main/java/com/example/jobtracker/controller/JobTrackerController.java
+++ backend/src/main/java/com/example/jobtracker/controller/JobTrackerController.java
@@ -7,23 +7,31 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/applications")
-@CrossOrigin(origins = "http://localhost:5173") // React frontend
+@RestController//This class handles REST API requests. Spring will automatically convert responses to JSON.
+@RequestMapping("/api/applications")//Base URL for all endpoints in this controller.
+@CrossOrigin(origins = "http://localhost:5173") // React frontend is allowed to call the backend.
+
 public class JobTrackerController {
 
     private final JobTrackerService service;
 
-    public JobTrackerController(JobTrackerService service) { this.service = service; }
+    public JobTrackerController(JobTrackerService service) { 
+        this.service = service; 
+    }
 
     @GetMapping
-    public List<JobApplication> getAll() { return service.findAll(); }
+    public List<JobApplication> getAll() { 
+        return service.findAll(); 
+    }
 
     @PostMapping
-    public JobApplication create(@RequestBody JobApplication jobApp) { return service.save(jobApp); }
+    public JobApplication create(@RequestBody JobApplication jobApp) { //takes JSON from the request body and convert it into a java object called jobApp
+        return service.save(jobApp); 
+    }
 
     @PutMapping("/{id}")
-    public JobApplication update(@PathVariable Long id, @RequestBody JobApplication details) {
+    public JobApplication update(@PathVariable Long id, //extracts ID from URL.
+                                 @RequestBody JobApplication details) {//takes JSON from the frontend request body and convert it into a java object called details
         return service.update(id, details);
     }
 
